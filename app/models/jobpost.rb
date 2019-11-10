41d2af563f9d3510407e8e3e4442b7317cadf046
Devin <devinwreeks@gmail.com>
2019-11-10 18:18:34 +1000

    nearing final updates before submission

--- app/models/jobpost.rb
+++ app/models/jobpost.rb
@@ -1,7 +1,8 @@
 class Jobpost < ApplicationRecord
     belongs_to :user
     has_one_attached :uploadedfile
-
+    validates :title, presence: true
+    validates :description, presence: true
     def can_edit?(user)
         return user == self.user || user.has_role?(:admin)
     end
